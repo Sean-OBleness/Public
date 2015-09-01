8424560893df8b3d3d9d7be3146030b5ffea8735
Sean O'Bleness <sean.m.obleness-1@ou.edu>
2015-08-31 20:40:06 -0500

    Controller

---
+++ SearchLine/src/SearchLineGUI.cpp
@@ -0,0 +1,17 @@
+#ifndef SEARCHLINEGUI_CPP
+#define SEARCHLINEGUI_CPP
+
+#include "BetterString.h"
+#include "Entry.h"
+#include "File.h"
+#include "FileList.h"
+#include "ArrayClass.h"
+#include "Vector.h"
+#include <ctype.h>
+#include <fstream>
+#include <unistd.h>
+#include <stdlib.h>
+
+
+
+#endif
